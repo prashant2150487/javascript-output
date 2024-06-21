e328f8f77ae57e967cec0cd74de948852b958838
Prashnt Sachan <swepra04@gmail.com>
2024-06-21 22:23:39 +0530

    q1 done

--- q1.js
+++ q1.js
@@ -1,1 +1,9 @@
-console.log("Hi")
+function sayHi(){
+    console.log(age)   // reference error 
+    console.log(name)   // undefind
+    var name="Prashant Sachan";
+    let age=23;
+
+    
+}
+sayHi()
