6db896adf04077542f840a095235f306521ee54c
Prashnt Sachan <swepra04@gmail.com>
2024-06-21 22:29:26 +0530

    q2.js

---
+++ q2.js
@@ -0,0 +1,26 @@
+for (let i = 0; i < 6; i++) {
+    setTimeout(() => {
+        console.log(i)
+    }, 0)
+}
+0
+1
+2
+3
+4
+5
+
+
+
+for (var i = 0; i < 6; i++) {
+    setTimeout(() => {
+        console.log(i)
+    }, 0)
+}
+
+6
+6
+6
+6
+6
+6
